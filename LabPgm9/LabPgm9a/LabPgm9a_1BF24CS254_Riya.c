9e37dfd67d2120983cd10d2dbbda175054bac0b3
riyamathewscs24 <riyamathews.cs24@bmsce.ac.in>
2025-12-22 09:36:38 +0530

    Add files via upload

---
+++ LabPgm9/LabPgm9a/LabPgm9a_1BF24CS254_Riya.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int graph[20][20], visited[20], n;
+
+void BFS(int start)
+{
+    int queue[20], front = 0, rear = 0;
+    visited[start] = 1;
+    queue[rear++] = start;
+
+    while (front < rear)
+    {
+        int node = queue[front++];
+        printf("%d ", node);
+
+        for (int i = 0; i < n; i++)
+        {
+            if (graph[node][i] == 1 && !visited[i])
+            {
+                visited[i] = 1;
+                queue[rear++] = i;
+            }
+        }                                    
+    }
+}
+
+int main()
+{
+    int start;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+        visited[i] = 0;
+
+    printf("Enter starting vertex: ");
+    scanf("%d", &start);
+
+    if (start < 0 || start >= n)
+    {
+        printf("Invalid starting vertex\n");
+        return 0;
+    }
+
+    printf("BFS Traversal: ");
+    BFS(start);
+
+    return 0;
+}
